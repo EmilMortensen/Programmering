586e03a99249c4aa73f1ed6f49a548196867000a
EmilMortensen <emilovic2004@gmail.com>
2021-10-11 12:49:33 +0200

    Lavet animation i css og opretter JSON

---
+++ 11-css-animation/index.js
@@ -0,0 +1,9 @@
+
+for(let i = 0; i<20; i++){
+    let card = document.createElement('div')
+    card.classList.add('card')
+    card.style.animation = 'anim';
+    card.style.animationDelay = i/40 + 's';
+    card.style.animationDuration = '.3s'
+    document.querySelector('main').append(card)
+}
